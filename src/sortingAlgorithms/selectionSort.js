d73bfc8c233ca143ecbb6bef225852591d682117
AlveeM <alvee.akand@outlook.com>
2020-09-08 23:13:51 -0400

    added selection sort

---
+++ src/sortingAlgorithms/selectionSort.js
@@ -0,0 +1,49 @@
+export default function selectionSortAnimations(arr) {
+  const animations = [];
+  if (arr.length <= 1) return arr;
+  selectionSort(arr, animations);
+  return animations;
+}
+
+function selectionSort(arr, animations) {
+  const n = arr.length;
+  for (let i=0; i < n-1; i++) {
+    let min_idx = i;
+    for (let j=i+1; j < n; j++) {
+      animations.push([min_idx, j]);
+      animations.push([min_idx, j]);
+      if (arr[j] < arr[min_idx]) {
+        min_idx = j;
+      }
+    }
+    [arr[i], arr[min_idx]] = [arr[min_idx], arr[i]];
+    animations.push([i, arr[i], true])
+    animations.push([min_idx, arr[min_idx], true])
+  }
+}
+
+const animations = [
+      [0, 1],
+      [0, 1],
+      [1, 2],
+      [1, 2],
+      [0, 400, true],
+      [1, 600, true],
+      [1, 2],
+      [1, 2],
+      [1, 500, true],
+      [2, 600, true],
+    ]
+
+function selectionSort1(arr) {
+  const n = arr.length;
+  for (let i=0; i < n-1; i++) {
+    let min_idx = i;
+    for (let j=i+1; j < n; j++) {
+      if (arr[j] < arr[min_idx]) {
+        min_idx = j;
+      }
+    }
+    [arr[i], arr[min_idx]] = [arr[min_idx], arr[i]];
+  }
+}
